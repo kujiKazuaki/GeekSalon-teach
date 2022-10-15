419ad5b84ad4d1d2f5b079ec7f8ae2a4a36f5473
kujiKazuaki <KujiKazuaki24@gmail.com>
2022-10-15 09:55:52 +0900

    week2-1

---
+++ Week1/Fizzbuzz/2-2/main.js
@@ -0,0 +1,5 @@
+// 2-2.fizzBuzz を、引数に受け取った数値をコンソールに表示するプログラムにしてください。試しに100 でfizzBuzzを実行してください。
+const fizzBuzz = function (number) {
+  return console.log(number);
+};
+fizzBuzz(100);
---
+++ Week1/Week1-2 オブジェクトで自己紹介しよう/README.md
@@ -0,0 +1,3 @@
+Week1-2 オブジェクトで自己紹介しよう
+URL
+https://www.notion.so/Week1-2-a474e51d621948968c4f79741d670027
